1f21860eadc66e80a72b7fd68ae82cc86ed69303
Khushibhardwaj07 <khushibhardwaj3535@gmail.com>
2025-04-01 01:44:04 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,42 +1,33 @@
 // Your code here...
 #include <stdio.h>
-
 int main() {
     int n;
     scanf("%d", &n);
     int arr[n];
-    for (int i = 0; i < n; i++) {
+    for(int i=0;i<n;i++) {
         scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j]>arr[j+1]) {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
-    }
-    int allSame = 1;
-    for (int i = 1; i < n; i++) {
-        if (arr[i] != arr[0]) {
-            allSame = 0;
-            break;
-        }
-    }
     
-    if (allSame) {
-        printf("-1");
-    } else {
-        int minValue = arr[n-2]; 
-        for (int i = n - 3; i >= 0; i--) {
-            if (arr[i] != arr[n-1]) {
-                minValue = arr[i];
+    }
+    int allEqual = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                allEqual = 0;
                 break;
             }
         }
-        printf("%d", minValue);
+    if (allEqual==0) {
+        printf("%d", arr[n-2]);
     }
-
-    return 0;
+    else {
+        printf("%d", -1);
+    }
 }
